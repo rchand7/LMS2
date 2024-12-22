8d977d9f2edf0a52505d90f7bb570707bc948e5c
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-12-23 04:00:07 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        // Using the connection string from environment variables (ensure it's set in .env)
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
     } catch (error) {
-        console.log("error occured", error); 
+        console.error('Error occurred while connecting to MongoDB:', error);
     }
 }
+
 export default connectDB;
