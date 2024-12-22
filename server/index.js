635ba9d51a1b5ac75bbec757f23a1ba921d9dda4
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-12-23 03:09:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,47 @@ import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
+import purchaseRoute from "./routes/purchaseCourse.route.js";import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import path from "path";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files from the 'client/dist' directory (adjusting path for correct location)
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB Connected');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+// Serve index.html for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 import courseProgressRoute from "./routes/courseProgress.route.js";
 import path from "path";
 
